b593314a9290270f58f3714c105bf017efdaa35e
MeeSh0o <ljhwuwu@qq.com>
2020-05-11 14:40:48 +0800

    forget what i did

---
+++ Assets/摄像机Follow/Scripts/摄像机F_控制器.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 摄像机F_控制器 : MonoBehaviour
+{
+    public float MoveForce = 3f;
+    public float JumpForce = 5f;
+    public Rigidbody RB;
+
+    private void Start() {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        float v = Input.GetAxis("Horizontal") * MoveForce;
+        RB.AddForce(new Vector3(v,0,0));
+
+        if(Input.GetButtonDown("Jump")){
+            RB.AddForce(new Vector3(0,JumpForce,0));
+            Debug.Log(Input.GetButtonDown("Jump"));
+        }
+    }
+}
+
+
